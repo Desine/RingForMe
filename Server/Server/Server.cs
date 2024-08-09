83ac932927a30e00e80c23e2a572360b2396f77e
Napolov Vlad <desine.main@gmail.com>
2024-08-09 13:45:26 +0300

    Created Server project

---
+++ Server/Server/Server.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Core;
+
+namespace Server;
+
+public class Server
+{
+    string localAddress;
+    Socket listenSocket;
+
+
+    public void Instantiate()
+    {
+        IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                localAddress = ip.ToString();
+                break;
+            }
+        }
+
+        listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    }
+
+    public async Task Accept()
+    {
+        while (true)
+        {
+            Socket acceptedSocket = await listenSocket.AcceptAsync();
+
+            _ = Task.Run(() => HandleClient(acceptedSocket));
+        }
+    }
+
+
+
+    public async Task HandleClient(Socket acceptedSocket)
+    {
+
+
+        Task receiveTask = ReceiveMessagesAsync(socket);
+        Task sendTask = SendMessagesAsync(socket);
+
+        await Task.WhenAny(receiveTask, sendTask);
+
+        socket.Shutdown(SocketShutdown.Both);
+        socket.Close();
+    }
+
+
+
+}
