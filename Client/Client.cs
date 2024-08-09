500c91d1791705c8747099b4ef1c7606e3fe0ebf
Napolov Vlad <desine.main@gmail.com>
2024-08-09 17:39:39 +0300

    Added Client project

---
+++ Client/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client;
+
+public class Client
+{
+
+    public Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    
+    public string serverAddress = "26.68.144.220";
+    public int serverPort = 2222;
+    public Server server = new();
+
+
+
+    public void Instantiate()
+    {
+        server.ipEndPoint = new IPEndPoint(IPAddress.Parse(serverAddress), serverPort);
+        server.socket = new(server.ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+    }
+
+
+    public async Task ConnectToServer(IPEndPoint iPEndPoint)
+    {
+        await clientSocket.ConnectAsync(server.ipEndPoint);
+        _ = Task.Run(() => server.reciever.RecieveMessageAsync(server.socket));
+    }
+
+
+}
---
+++ Client/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client;
+
+public class Server
+{
+    public IPEndPoint ipEndPoint;
+    public Socket socket;
+    public Core.Reciever reciever = new();
+    public Core.Sender sender = new();
+
+}
