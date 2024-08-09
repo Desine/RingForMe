494cafc1f02eb4ade45da19cca6ed8b1cf384693
Napolov Vlad <desine.main@gmail.com>
2024-08-09 21:54:45 +0300

    Added ServerConsoleUI project

---
+++ ServerConsoleUI/ServerConsoleUI.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+Server.Server server = new();
+server.Instantiate();
+_ = Task.Run(() => server.Accept());
+
+
+while (true)
+{
+    Console.WriteLine("Options:\b" +
+        "1 - Send message");
+
+    int choise = Console.Read();
+    switch (choise)
+    {
+        case 1:
+            SendMessage();
+            break;
+    }
+}
+
+
+
+
+void SendMessage()
+{
+    Console.Write("Send to: ");
+    string recipient = Console.ReadLine();
+    Console.Write("Message: ");
+    string message = Console.ReadLine();
+
+    server.SendMessage("me", "Hi, me");
+
+}
+
+
+
