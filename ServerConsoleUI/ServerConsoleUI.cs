ed060e541d8d905e395c2b6c0bc3d876662d8458
Napolov Vlad <desine.main@gmail.com>
2024-08-10 18:18:31 +0300

    Added more commands

--- ServerConsoleUI/ServerConsoleUI.cs
+++ ServerConsoleUI/ServerConsoleUI.cs
@@ -17,14 +17,27 @@ void WriteReceivedMessage(string message)
 
 while (true)
 {
-    Console.WriteLine("Options:\n" +
-        "1 - Send message");
+    Console.WriteLine("Options:" +
+        "\n1 - Send message to client" +
+        "\n2 - Send message to all clients" +
+        "\n3 - Connected clients" +
+        "\n4 - Server info"
+        );
 
     string choise = Console.ReadLine();
     switch (choise)
     {
         case "1":
-            SendMessage();
+            SendMessageToClient();
+            break;
+        case "2":
+            SendMessageToAllClients();
+            break;
+        case "3":
+            ConnectedClients();
+            break;
+        case "4":
+            ServerInfo();
             break;
     }
 }
@@ -32,7 +45,7 @@ while (true)
 
 
 
-void SendMessage()
+void SendMessageToClient()
 {
     Console.Write("Send to: ");
     string recipient = Console.ReadLine();
@@ -41,7 +54,27 @@ void SendMessage()
 
     server.SendMessageToClient(recipient, message);
 }
+void SendMessageToAllClients()
+{
+    Console.Write("Message: ");
+    string message = Console.ReadLine();
 
+    server.SendMessageToAllClients(message);
+}
+
+void ConnectedClients()
+{
+    foreach (var client in server.clients)
+    {
+        Console.WriteLine("client namae: " + client.name);
+        Console.WriteLine("client socket: " + client.socket);
+    }
+}
+void ServerInfo()
+{
+        Console.WriteLine("Server local address: " + server.localAddress);
+        Console.WriteLine("Server socket: " + server.listenSocket);
+}
 
 
 
