e4e70e05a846d7c7bf287e571ce1fe4a443697cf
Napolov Vlad <desine.main@gmail.com>
2024-08-10 17:56:55 +0300

    Client should be able to connect to server

--- ClientConsoleUI/ClientConsoleUI.cs
+++ ClientConsoleUI/ClientConsoleUI.cs
@@ -1,1 +1,38 @@
 ﻿
+
+Client.Client client = new();
+client.Instantiate();
+_ = Task.Run(()=> client.ConnectToServer(client.server.ipEndPoint));
+
+
+
+while (true)
+{
+    Console.WriteLine("Options:\n" +
+        "1 - Send message");
+
+    string choise = Console.ReadLine();
+    switch (choise)
+    {
+        case "1":
+            SendMessage();
+            break;
+    }
+}
+
+
+
+
+void SendMessage()
+{
+    Console.Write("Send to: ");
+    string recipient = Console.ReadLine();
+    Console.Write("Message: ");
+    string message = Console.ReadLine();
+
+    client.server.sender.SendMessage(client.server.socket, message);
+}
+
+
+
+
