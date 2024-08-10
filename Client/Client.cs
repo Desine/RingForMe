3c0e0ff1c1b9f3a9702ad0c52e1ab210e543ae7b
Napolov Vlad <desine.main@gmail.com>
2024-08-10 18:26:36 +0300

    Client/Server responsibility fix

--- Client/Client.cs
+++ Client/Client.cs
@@ -9,24 +9,17 @@ namespace Client;
 public class Client
 {
 
-    public Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    public Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
     
-    public string serverAddress = "26.68.144.220";
-    public int serverPort = 2222;
     public Server server = new();
 
 
 
-    public void Instantiate()
-    {
-        server.ipEndPoint = new IPEndPoint(IPAddress.Parse(serverAddress), serverPort);
-        server.socket = new(server.ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-    }
-
-
     public async Task ConnectToServer(IPEndPoint iPEndPoint)
     {
-        await clientSocket.ConnectAsync(server.ipEndPoint);
+        server.ipEndPoint = new IPEndPoint(IPAddress.Parse(server.address), server.port);
+        server.socket = new(server.ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        await socket.ConnectAsync(server.ipEndPoint);
         _ = Task.Run(() => server.receiver.ReceiveMessageAsync(server.socket));
     }
 
