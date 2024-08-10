bb762c81e327dae6f340b6b6a8073e5f90ee6e8a
Napolov Vlad <desine.main@gmail.com>
2024-08-10 19:12:41 +0300

    Added Action OnConnectedToServer

--- Client/Client.cs
+++ Client/Client.cs
@@ -10,9 +10,9 @@ public class Client
 {
 
     public Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-    
-    public Server server = new();
 
+    public Server server = new();
+    public Action OnConnectedToServer = () => { };
 
 
     public async Task ConnectToServer(IPEndPoint iPEndPoint)
@@ -20,6 +20,7 @@ public class Client
         server.ipEndPoint = new IPEndPoint(IPAddress.Parse(server.address), server.port);
         server.socket = new(server.ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
         await socket.ConnectAsync(server.ipEndPoint);
+        OnConnectedToServer();
         _ = Task.Run(() => server.receiver.ReceiveMessageAsync(server.socket));
     }
 
