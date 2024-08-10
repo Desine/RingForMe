d4cb0aab88483b12897c286881f0080087a6edaf
Napolov Vlad <desine.main@gmail.com>
2024-08-10 18:34:52 +0300

    Added more commands

--- ClientConsoleUI/ClientConsoleUI.cs
+++ ClientConsoleUI/ClientConsoleUI.cs
@@ -1,21 +1,33 @@
 ﻿
 
 Client.Client client = new();
-client.Instantiate();
 _ = Task.Run(()=> client.ConnectToServer(client.server.ipEndPoint));
 
 
 
 while (true)
 {
-    Console.WriteLine("Options:\n" +
-        "1 - Send message");
+    Console.WriteLine("Options:" +
+        "\n1 - Send message" +
+        "\n2 - Serve info");
 
     string choise = Console.ReadLine();
     switch (choise)
     {
         case "1":
             SendMessage();
+            break;
+        case "2":
+            ServerInfo();
+            break;
+        case "3":
+            ChengeServerAdress();
+            break;
+        case "4":
+            ChengeServerPort();
+            break;
+        case "5":
+            ConnectToServer();
             break;
     }
 }
@@ -25,14 +37,32 @@ while (true)
 
 void SendMessage()
 {
-    Console.Write("Send to: ");
-    string recipient = Console.ReadLine();
     Console.Write("Message: ");
     string message = Console.ReadLine();
 
     client.server.sender.SendMessage(client.server.socket, message);
 }
 
+void ServerInfo()
+{
+    Console.WriteLine();
+}
 
+void ChengeServerAdress()
+{
+    Console.Write("Server adress: ");
+    string address = Console.ReadLine();
+    client.server.address = address;
+}
 
+void ChengeServerPort()
+{
+    Console.Write("Server port: ");
+    int port = int.Parse(Console.ReadLine());
+    client.server.port = port;
+}
+void ConnectToServer()
+{
+    client.ConnectToServer(client.server.ipEndPoint);
+}
 
