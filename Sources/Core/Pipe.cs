003ee5df1c236a0ee5dc5c71cfe6cb70dc40d55f
Napolov Vlad <85844885+Desine@users.noreply.github.com>
2024-09-15 14:09:54 +0300

    added using

--- Sources/Core/Pipe.cs
+++ Sources/Core/Pipe.cs
@@ -1,3 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+
+
 namespace Core;
 
 
